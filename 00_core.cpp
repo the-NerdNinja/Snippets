ea57016e6102ffaca5f676d79ac422293565a5c9
Mahiul Kabir <mahiulkabir@outlook.com>
2024-06-09 02:18:33 +0600

    Commit #0

---
+++ 00_core.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<ll>;
+using pii = pair<ll, ll>;
+using grid = vector<vi>;
+
+#define sz(_O) _O.size()
+#define fix(_O) cout<<setprecision(_O)<<fixed
+#define fir(_O) for(int i=0; i<_O; ++i)
+#define fjr(_O) for(int j=0; j<_O; ++j)
+
+ll const inf = 2e9;
+ll const mod = 998244353;
+
+
+void solve(){
+  
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tt = 1; cin>>tt;
+  while(tt--) solve();
+}
+
