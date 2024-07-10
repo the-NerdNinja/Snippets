a07e1b7ed0c79d3c2fcb360d815b07aa3458afa6
Mahiul Kabir <mahiulkabir@outlook.com>
2024-07-10 15:56:49 +0600

    Update 00_core.cpp

--- 00_core.cpp
+++ 00_core.cpp
@@ -1,19 +1,26 @@
 
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 using ll = long long;
 using vi = vector<ll>;
 using pii = pair<ll, ll>;
 using grid = vector<vi>;
 
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, 
+                         tree_order_statistics_node_update>; 
+#define en "\n"
 #define sz(_O) _O.size()
 #define fix(_O) cout<<setprecision(_O)<<fixed
 #define fir(_O) for(int i=0; i<_O; ++i)
 #define fjr(_O) for(int j=0; j<_O; ++j)
 
-ll const inf = 2e9;
-ll const mod = 998244353;
+ll const inf = LLONG_MAX; //0x3f3f3f3f3f3f;
+ll const mod = 998244353; //1e9+7
 
 
 void solve(){
